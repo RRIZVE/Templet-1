74d2e93efa958f5f1574b2e2b10d2b1a02b71a7d
RIZVE <62658788+RRIZVE@users.noreply.github.com>
2022-04-15 01:00:15 +0600

    Add files via upload

---
+++ 1 Templet/vector arrey 3 index.cpp
@@ -0,0 +1,99 @@
+//***********************Bismillah-ir-Rahman-ir-Rahim******************************\
+//In the Name of Allah
+//La ilaha illallah Muhammadur Rasulullah
+//Rabbir Hamhuma Kama Rabba Yani Sagheera
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define Alhamdulillah ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vs vector<string>
+#define pll pair<ll,ll>
+#define vp vector<pair<ll,ll>>
+#define pb push_back
+#define ff first
+#define ss second
+#define pi acos(-1.0)
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define EPS 1e-9
+
+ll ceilAB(ll a, ll b) { return (a + b - 1) / b; }
+ll Pow(ll c, ll d) {return d == 0 ? 1 : c * pow(c, d - 1);}
+ll gcd(ll a, ll b) {return b == 0 ? a : gcd(b, a % b);}
+ll lcm(ll a, ll b) {return ((a * b) / gcd(a, b));}
+
+const int mx = 1e6;
+int a[mx];
+
+
+
+void solve()
+{
+
+	int i, j, k , n, m, x, y, z, c = 0, cnt = 0;
+	string s;
+
+	vector<array<int, 3>> v;
+
+	cin >> n;
+
+	for ( i = 1; i <= n ; i++)
+	{
+		cin >> x >> y >> z;
+		v.pb({x, y, z});
+
+
+	}
+	sort(v.begin(), v.end());
+	for ( i = 0; i < v.size() ; i++)
+	{
+		cout << v[i][0] << " " << v[i][1] << " " << v[i][2] << endl;
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+int main()
+{
+
+	Alhamdulillah;
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	solve();
+	return 0;
+
+}
